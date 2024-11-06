e8156510668cee40ed8c05c2a73b0eca6f5d369e
JoseMarioCarranza <imt_josecarranza@outlook.com>
2024-11-06 16:47:51 -0700

    creación de componentes basicos

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css'
+import { Dashboard, Home, Loggin, Secret, Signup } from '@/Pages'
+import Header from '@/Components/Header/Header'
 
 function App() {
 
   return (
     <>
+      <Header />
       <p>React Auth</p>
+      <Home />
+      <Dashboard />
+      <Loggin />
+      <Secret />
+      <Signup />
     </>
   )
 }
---
+++ src/Pages/index.js
@@ -0,0 +1,17 @@
+import Dashboard from '@/Pages/Dashboard'
+import Home from '@/Pages/Home'
+import Loggin from '@/Pages/Loggin'
+import Secret from '@/Pages/Secret'
+import Signup from '@/Pages/Signup'
+
+/* This code snippet is using the ES6 module syntax to export multiple components from different files.
+The `export { ... }` syntax is used to export named exports from a module. In this case, it is
+exporting the components `Dashboard`, `Home`, `Loggin`, `Secret`, and `Signup` from their respective
+files so that they can be imported and used in other parts of the application. */
+export {
+    Dashboard,
+    Home,
+    Loggin,
+    Secret,
+    Signup
+}
